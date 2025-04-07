2c36f361133dda475fdc787fab086c4ef6c81181
awsrcld04 <akil.woolfolk@gmail.com>
2025-04-06 21:26:57 -0400

    Remove old AccountLogonCheck.cs file copyright header

--- AccountLogonCheck.cs
+++ AccountLogonCheck.cs
@@ -1,9 +1,4 @@
-﻿//****************************************
-// Copyright (c) Thinkability Group 2011
-// SystemsAdminPro.com
-//****************************************
-
-//Description:
+﻿//Description:
 //Finds accounts that have not logged in within a certain number of days
 //Disables accounts that have not logged in within a certain number of days
 //Optional: Find and disable accounts that have not been used (no login datetimestamp)
